77e05177751d51e379c38f0961965853b5d01736
suyashpotdar <suyashpotdar03@gmail.com>
2024-10-08 11:52:26 +0530

    deploy mlflow

--- steps/evaluation.py
+++ steps/evaluation.py
@@ -1,12 +1,16 @@
 import logging
+import mlflow
 import pandas as pd
 from zenml import step
 from src.evaluation import MSE, RMSE, R2
 from sklearn.base import RegressorMixin
 from typing import Tuple
 from typing_extensions import Annotated
+from zenml.client import Client
 
-@step
+experiment_tracker = Client().active_stack.experiment_tracker
+
+@step(experiment_tracker=experiment_tracker.name)
 def evaluate_model(
     model: RegressorMixin, 
     X_test: pd.DataFrame,
@@ -24,12 +28,15 @@ def evaluate_model(
         prediction = model.predict(X_test)
         mse_class = MSE()
         mse = mse_class.calculate_scores(y_test, prediction)
+        mlflow.log_metric("mse", mse)
 
         r2_class = R2()
         r2 = r2_class.calculate_scores(y_test, prediction)
+        mlflow.log_metric("r2", r2)
 
         rmse_class  = RMSE()
         rmse = rmse_class.calculate_scores(y_test, prediction)
+        mlflow.log_metric("rmse", rmse)
 
         return r2, rmse
     except Exception as e:
